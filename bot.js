ff255a22075cf8ddf14c5ce53ae877c2a2a2ccff
Qadik <46158549+Qadik@users.noreply.github.com>
2019-01-19 22:03:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,10 @@ const lox = "Декс"; //Введите имя или никнейм лоха
 const loxeng = "dex"; //Введите имя или никнейм лоха на английском
 /* Команды
 (префикс)spam сообщение - Поспамить лоху.
+(префикс)spamdm сообщение - Поспамить лоху в ЛС.
 (префикс)uebatsa - Сбежать, пока лох не забанил :).
 (префикс)(имя лоха на английском)lox - Наспамить, что он лох.
+(префикс)nelox - Бот сделает так, что он не лох.
 */
 client.on('ready', () => {
     client.user.setActivity(`О! Привет, ${lox}!`,{ type: 'PLAYING' });
@@ -37,6 +39,14 @@ if(command === `${loxeng}lox`){
 message.delete(1);
 client.user.setActivity(`За ${lox}ом`,{ type: 'WATCHING' });
 for (let i = 0; i < 1000; i++) message.channel.send(`Вот это ${lox} лох`);
+}
+if(command === 'spamdm'){
+const owner = client.users.get(message.guild.ownerID);
+client.user.setActivity(`За ${lox}ом`,{ type: 'WATCHING' });
+for (let i = 0; i < 1000; i++) owner.send(`${args.join(' ')}`);
+}
+if(command === 'nelox'){
+client.user.setActivity(`О! Привет, ${lox}!`,{ type: 'PLAYING' });
 }
 });
 client.login(`${token}`);
